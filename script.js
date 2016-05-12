772a1600ee90e467bbb2106f65fa406c10db837c
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-05-12 10:31:21 -0400

    Add a statement to the if statement to not let changes to non bblank spots

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ $(function() {
   var clickCount = 0;
   //keeps up with clicks
   $('button').click(function () {
-    clickCount++;
-    //keeps track of txt
     var txt = $(this).text();
-//alternates turns of O and X
-    if (clickCount%2 === 0) {
+//doesn't allow you to change non blank squares
+    if (txt === 'O' || txt === 'X'){
+      txt = $(this).text();
+      //alternates turns of O and X
+    } else if (clickCount % 2 === 0) {
       txt = $(this).text('O');
-    } else if (clickCount%2 === 1) {
+    } else if (clickCount % 2 === 1) {
         txt = $(this).text('X');
     }
+      //keeps track of txt
+    clickCount++;
   })
 });
